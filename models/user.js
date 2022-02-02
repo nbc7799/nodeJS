5b09fe0bcc71f0ba010289674b8d56da31ffaf98
nbc7799 <nbc7799@naver.com>
2022-02-02 23:01:19 +0900

    node.js

---
+++ models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    trim: true,
+    unique: 1,
+  },
+  password: {
+    type: String,
+    maxlength: 50,
+  },
+  role: {
+    type: Number,
+    default: 0,
+  },
+  image: String,
+  token: {
+    type: String,
+  },
+  tokenExp: {
+    type: Number,
+  },
+});
+//model은 Schema를 감싸주는 역할, Schema는 user의 정보들을 지정해줄수있음
+const User = mongoose.model("User", userSchema);
+
+module.exports = { User };
